4917811cc9ce8a74ea9e59515a7f7825421d8a92
Hamza Anjum <66873696+hamza-create@users.noreply.github.com>
2022-10-24 20:30:57 +0500

    Add files via upload

---
+++ fruits/ch4_ex1fruits.py
@@ -0,0 +1,7 @@
+f1= input("enter fruit name")
+f2= input("enter fruit name")
+f3= input("enter fruit name")
+f4= input("enter fruit name")
+f5= input("enter fruit name")
+fruitlist= [f1, f2, f3, f4, f5]
+print(fruitlist)
